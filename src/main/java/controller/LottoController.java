63a97d7f395627fbea36ae1f965d4b26fab3b9aa
pci2676 <pci2676@gmail.com>
2019-03-22 16:02:16 +0900

    컨트롤러 테스트만하고 안만들어놨었음.....

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -2,11 +2,20 @@ package controller;
 
 import domain.LottoList;
 import dto.LottoResultDto;
+import view.InputView;
 import view.ResultView;
 
 import java.util.List;
 
 public class LottoController {
+
+    public String start(){
+        InputView inputView = new InputView(System.in);
+        LottoList lottoList = inputView.getLottoList();
+        List<Integer> winningNumbers = inputView.getWinningNumbers();
+
+        return lotto(lottoList,winningNumbers);
+    }
 
     public String lotto(LottoList lottoList, List<Integer> winningNumbers) {
 
